d4da6a0be978856cea0f63349514c1275214ab00
wuzhihua <wuzhihua0809@gmail.com>
2016-05-16 17:52:02 +0800

    no message

--- script.js
+++ script.js
@@ -5,54 +5,70 @@ $(document).ready(function() {
         window.location.href = url;
         $('.content').val() = "";
     }
-    $('.search').click(function(){
+    $('.search').click(function() {
         read();
     });
-    $('.feeling').click(function(){
+    $('.feeling').click(function() {
         window.location.href = "https://www.google.com/doodles/";
     });
-    $('.content').keydown(function(){
-        if(event.keyCode =='13'){
+    $('.content').keydown(function() {
+        if(event.keyCode =='13') {
             read();
         }
     });
-    $('.nine').click(function(){
+    $('.nine').click(function() {
         if ($('.hide').css('display') === "none") {
             $('.hide').show();
+            $('.triangle').show();
             $('.extrabar').show();
             $('.extracontent').hide();
             $('.separator').hide();
-            $('extra').css('height','440px');
-        } else{
+        } else {
             $('.hide').hide();
+            $('.triangle').hide();
         };
         event.stopPropagation(); 
     });
-    $('.hide').click(function(){
+    $('.hide').click(function() {
         event.stopPropagation(); 
     });
-    $('.mic').mouseover(function(){
+    $('.triangle').click(function() {
+        event.stopPropagation(); 
+    });
+    $('.mic').mouseover(function() {
         $('.smalltriangle').show();
         $('.note').show();
     });
-    $('.mic').mouseout(function(){
+    $('.mic').mouseout(function() {
         $('.smalltriangle').hide();
         $('.note').hide();
     });
-    $(document).click(function(){
+    $(document).click(function() {
         $('.hide').hide();
+        $('.triangle').hide();
     });
-    // $('extra').scroll(function(){
-    //     if ($(extra.scrollTop()>0)) {
-    //         $('.extrabar').hide();
-    //         $('.extracontent').show();
-    //         $('.separator').show();
-    //     } ;
-    // });
-    $('.extrabar').click(function(){
+    $('.hide').scroll(function() {
+        if ($('.hide').scrollTop() > 0) {
+            $('.extrabar').hide();
+            $('.extracontent').show();
+            $('.separator').show();
+            if ($('.hide').scrollTop() == 320) {
+                $('.extracontent').css('border-bottom','1px solid #cccccc');
+                $('.hide').css('border-bottom','0px');
+            } else {
+                $('.hide').css('border-bottom','1px solid #cccccc');
+            };
+        } else {
+            $('.extrabar').show();
+            $('.extracontent').hide();
+            $('.separator').hide();
+        };
+    });
+    $('.extrabar').click(function() {
         $('.extrabar').hide();
         $('.extracontent').show();
         $('.separator').show();
-        $('extra').css('height','800px');
+        $('.hide').scrollTop(320);
+        $('.extracontent').css('border-bottom','1px solid #cccccc');
     });
 });
